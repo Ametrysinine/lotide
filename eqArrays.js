ebdea8edc6c231d79880f58a4dbae3dfb787c0c7
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-25 14:02:43 -0800

    Added eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
+  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
+
+  if (actual === expected) {
+    console.log(passMessage);
+
+  } else {
+    console.log(failMessage);
+
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let arraysEqual = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    arraysEqual = false;
+
+  } else {
+    for (let i in arrayOne) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        arraysEqual = false;
+      }
+
+    }
+
+    return arraysEqual;
+
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
