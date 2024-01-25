8de13cf80ae244d414b21c92c3a6a615d16fd708
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-24 21:35:19 -0800

    Created head function

---
+++ head.js
@@ -0,0 +1,29 @@
+//assertEqual import start
+
+const assertEqual = function(actual, expected) {
+
+  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
+  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
+
+  if (actual === expected) {
+    console.log(passMessage);
+
+  } else {
+    console.log(failMessage);
+
+  }
+};
+
+//assertEqual import end
+
+const head = function(array) {
+  if (Array.isArray(array)) {
+    return array[0];
+  } else {
+    return "This is not an array.";
+  }
+
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
