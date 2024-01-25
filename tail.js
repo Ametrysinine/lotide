3214cf417094d9dee313cafdbdf1b556b2dca845
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-24 21:49:28 -0800

    Added tail function

---
+++ tail.js
@@ -0,0 +1,34 @@
+//assertEqual import start
+
+const assertEqual = function(actual, expected) {
+
+  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
+  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
+
+  if (actual === expected) {
+    console.log(passMessage);
+
+  } else {
+    console.log(failMessage);
+
+  }
+};
+
+//assertEqual import end
+
+const tail = function(array) {
+  let tailArray = [];
+  if (Array.isArray(array)) {
+    for (let i = 1; i < array.length; i++) {
+      tailArray.push(array[i]);
+    }
+    return tailArray;
+
+  } else {
+    return "This is not an array.";
+  }
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
