e817ced52885fe44a4aa9555f93db9527c88c3b1
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-25 15:55:53 -0800

    Drafted without function

---
+++ without.js
@@ -0,0 +1,24 @@
+const without = function(array, itemsToRemove) {
+  let cleanedArray = [];
+  let removalIndex = 0;
+  let i = 0;
+
+  while (i < array.length) {
+    if (removalIndex === itemsToRemove.length) {
+      cleanedArray.push(array[i]);
+      i++;
+      removalIndex = 0;
+
+    } else if (array[i] === itemsToRemove[removalIndex]) {
+      i++;
+    } else {
+      removalIndex++;
+    }
+  
+
+  }
+
+  return cleanedArray;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
