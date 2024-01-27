8e47b486b18ff54bccc4fa1a8d9bcd183f2f8c60
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-26 21:03:41 -0800

    Converted test cases to use assertArraysEqual

--- without.js
+++ without.js
@@ -57,8 +57,8 @@ const without = function(array, blocklist) {
   return cleanArray;
 };
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-console.log(without(["11", "11", "23"], ["11"])); // => ["23"]
-console.log(without(["4", "4", "4"], ["4"])); // => []
-console.log(without(["50", "25", "50", "10", "30"], ["50", "10"])); // => ["25", "30"]
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // Pass
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // Pass
+assertArraysEqual(without(["11", "11", "23"], ["11"]), ["23"]); // Pass
+assertArraysEqual(without(["4", "4", "4"], ["4"]), []); // Pass
+assertArraysEqual(without(["50", 25, "50", "10", "30"], ["50", "10"]), [25, "30"]); // Pass
