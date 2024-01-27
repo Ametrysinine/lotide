fdb738b378085b9396f2f1329f28e4107844d869
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-01-26 20:56:25 -0800

    Import assertArraysEqual, eqArrays

--- without.js
+++ without.js
@@ -1,3 +1,33 @@
+const assertArraysEqual = function(actualArray, expectedArray) { // Import assertArraysEqual
+  const passMessage = `✔✔ Test passed ✔✔: ${actualArray} === ${expectedArray}`;
+  const failMessage = `❌ Test failed ❌: ${actualArray} !== ${expectedArray}`;
+
+  if (eqArrays(actualArray, expectedArray)) {
+    console.log(passMessage);
+
+  } else {
+    console.log(failMessage);
+  }
+  
+};
+
+const eqArrays = function(arrayOne, arrayTwo) { // Import eqArrays
+  let arraysEqual = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+
+  } else {
+    for (let i in arrayOne) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        arraysEqual = false;
+      }
+
+    }
+  }
+  return arraysEqual;
+};
+
 const existsInArray = function(item, array) { // Checks if item is in array
   let i = 0;
 
