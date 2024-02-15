0cb4f3ee71032c8a7c29a4e908649d1a73265cb0
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-14 23:19:46 -0800

    Refactored middle

--- middle.js
+++ middle.js
@@ -1,36 +1,3 @@
-const assertArraysEqual = function(actualArray, expectedArray) { // Import assertArraysEqual
-
-  const passMessage = `✔✔ Test passed ✔✔: ${actualArray} === ${expectedArray}`;
-  const failMessage = `❌ Test failed ❌: ${actualArray} !== ${expectedArray}`;
-
-  if (eqArrays(actualArray, expectedArray)) {
-    console.log(passMessage);
-
-  } else {
-    console.log(failMessage);
-  }
-
-};
-
-const eqArrays = function(arrayOne, arrayTwo) { // Import eqArrays
-  let arraysEqual = true;
-
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-
-  } else {
-    for (let i in arrayOne) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        arraysEqual = false;
-      }
-
-    }
-  }
-  return arraysEqual;
-};
-
-
-
 const middle = function(array) {
   if (array.length === 1 || array.length === 2) { // If array is length 1 or 2, return empty array
     return [];
@@ -40,10 +7,4 @@ const middle = function(array) {
   }
 };
 
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Pass
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Pass
-assertArraysEqual(middle([1, 2, 3]), [2]); // Pass
-assertArraysEqual(middle([1, 2]), []); // Pass
-assertArraysEqual(middle([1]), []); // Pass
+module.exports = middle;
