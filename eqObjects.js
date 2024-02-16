6551dab2e1324e9a6b113f7d8a8c0b0db2d62222
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-15 20:53:50 -0800

    Cleaned eqObjects for export

--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,4 @@
-const eqArrays = function(arrayOne, arrayTwo) { // Import eqArrays
-  let arraysEqual = true;
-
-  if (arrayOne.length !== arrayTwo.length) {
-    arraysEqual = false;
-
-  } else {
-    for (let i in arrayOne) { // Compare each item in array
-      if (arrayOne[i] !== arrayTwo[i]) {
-        arraysEqual = false;
-      }
-    }
-  }
-
-  return arraysEqual;
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(objectOne, objectTwo) { // eqObjects start
   if (Object.keys(objectOne).length !== Object.keys(objectTwo).length) { // Length checker
@@ -33,26 +18,11 @@ const eqObjects = function(objectOne, objectTwo) { // eqObjects start
     }
 
     if (objectOne[keys[i]] !== objectTwo[keys[i]]) { // Primitive checker
-      console.log(objectTwo[keys[i]]);
       return false;
     }
   }
 
   return true;
-}; // eqObjects end
-
-
-const objectWithArrays = {
-  1: [2, 3, 4]
 };
-const object2WithArrays = {
-  1: [2, 3]
-};
-console.log(eqObjects(objectWithArrays, object2WithArrays));
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)); // => true
-
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-console.log(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject)); // => false
+module.exports = eqObjects;
