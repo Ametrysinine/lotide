1a8fb3f857529c24ee9e3ce7fd6a156e1f7fc857
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-15 21:11:21 -0800

    without.js: Nested helper function, cleaned for export

--- without.js
+++ without.js
@@ -1,53 +1,25 @@
-const assertArraysEqual = function(actualArray, expectedArray) { // Import assertArraysEqual
-  const passMessage = `✔✔ Test passed ✔✔: ${actualArray} === ${expectedArray}`;
-  const failMessage = `❌ Test failed ❌: ${actualArray} !== ${expectedArray}`;
-
-  if (eqArrays(actualArray, expectedArray)) {
-    console.log(passMessage);
-
-  } else {
-    console.log(failMessage);
-  }
-  
-};
-
-const eqArrays = function(arrayOne, arrayTwo) { // Import eqArrays
-  let arraysEqual = true;
-
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-
-  } else {
-    for (let i in arrayOne) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        arraysEqual = false;
-      }
-
-    }
-  }
-  return arraysEqual;
-};
-
-const existsInArray = function(item, array) { // Checks if item is in array
-  let i = 0;
-
-  while (i < array.length) {
-    if (item === array[i]) {
-      return true;
-
-    } else {
-      i++;
-    }
-  }
-
-  if (i === array.length) { // Return true after every comparing to every index in array
-    return false;
-  }
-};
-
 const without = function(array, blocklist) {
+  const existsInArray = function(item, array) { // Checks if item is in array, helper function
+    let i = 0;
+  
+    while (i < array.length) {
+      if (item === array[i]) {
+        return true;
+  
+      } else {
+        i++;
+      }
+    }
+  
+    if (i === array.length) { // Return true after every comparing to every index in array
+      return false;
+    }
+  };
+  
+  
   let cleanArray = [];
 
+  
   for (let i = 0; i < array.length; i++) {
     if (!existsInArray(array[i], blocklist)) { // Add to result array if not in blocklist
       cleanArray.push(array[i]);
@@ -57,8 +29,4 @@ const without = function(array, blocklist) {
   return cleanArray;
 };
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // Pass
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // Pass
-assertArraysEqual(without(["11", "11", "23"], ["11"]), ["23"]); // Pass
-assertArraysEqual(without(["4", "4", "4"], ["4"]), []); // Pass
-assertArraysEqual(without(["50", 25, "50", "10", "30"], ["50", "10"]), [25, "30"]); // Pass
+module.exports = without;
