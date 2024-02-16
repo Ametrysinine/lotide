ffccfde21063db91ca2bc27db4545d26e64a693a
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-15 20:55:16 -0800

    Cleaned findKey for export

--- findKey.js
+++ findKey.js
@@ -1,20 +1,6 @@
-const assertEqual = function(actual, expected) { // Import assertEqual
-
-  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
-  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
-
-  if (actual === expected) {
-    console.log(passMessage);
-
-  } else {
-    console.log(failMessage);
-
-  }
-};
-
-let findKey = function(object, callback) {
-  let keys = Object.keys(object);
-  let values = Object.values(object);
+const findKey = function(object, callback) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
 
   for (let i = 0; i < values.length; i++) {
     if (callback(values[i])) {
@@ -25,20 +11,4 @@ let findKey = function(object, callback) {
   return undefined;
 };
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3), "Akaleri");
+module.exports = findKey;
