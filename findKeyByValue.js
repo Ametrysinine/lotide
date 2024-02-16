2d156ba8670ac96b66f84561000dc8b0ea0c1adb
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-15 20:56:04 -0800

    Cleaned findKeyByValue for export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,20 +1,6 @@
-const assertEqual = function(actual, expected) { // Import assertEqual
-
-  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
-  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
-
-  if (actual === expected) {
-    console.log(passMessage);
-
-  } else {
-    console.log(failMessage);
-
-  }
-};
-
-let findKeyByValue = function(object, value) {
-  let keys = Object.keys(object);
-  let values = Object.values(object);
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
 
   for (let i = 0; i < values.length; i++) {
     if (values[i] === value) {
@@ -23,14 +9,6 @@ let findKeyByValue = function(object, value) {
   }
 
   return undefined;
-}
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+module.exports = findKeyByValue;
