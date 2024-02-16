b6d8da59e30b356907eab7bdfea558824329747a
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-15 20:48:16 -0800

    Cleaned countOnly for export

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,3 @@
-const assertEqual = function(actual, expected) { // Import assertEqual
-
-  const passMessage = `✔✔ Test passed ✔✔: ${actual} === ${expected}`;
-  const failMessage = `❌ Test failed ❌: ${actual} !== ${expected}`;
-
-  if (actual === expected) {
-    console.log(passMessage);
-
-  } else {
-    console.log(failMessage);
-
-  }
-};
-
 const countOnly = function(allItems, itemsToCount) {
   const stringCount = {};
   const keys = Object.keys(itemsToCount);
@@ -37,24 +23,4 @@ const countOnly = function(allItems, itemsToCount) {
   return stringCount;
 };
 
-const criteria = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-countOnly(firstNames, criteria);
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
