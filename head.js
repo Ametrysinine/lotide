7925098cbdb77aa1e30eeb3ff53e3d73c34ad058
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-20 16:00:04 -0800

    Corrected tail.js failure to be undefined

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ const head = function(array) {
   if (Array.isArray(array)) {
     return array[0]; // Return first element of array
   } else {
-    return "This is not an array.";
+    return undefined;
   }
 
 };
